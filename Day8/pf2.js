0c80cda140295d9abd81ca3f790a866ee92ad912
007divyanshu <pandeydivyanshu1509@gmail.com>
2024-11-04 10:25:00 +0530

    fetch git data

---
+++ Day8/pf2.js
@@ -0,0 +1,29 @@
+async function orderfood(name,time){
+    return new Promise((resolve,reject)=>{
+        if (name == "burger") reject("burger not available");
+        setTimeout(()=>{
+            console.log("order"+ name +"prepared")
+        },time);
+    });
+    }
+    
+    async function restraunt(){
+        console.log("order placed");
+        const pizza= await orderfood("pizza",1000);
+        console.log(pizza);
+        try{
+            const burger= await orderfood("burger",2000);
+            console.log(burger);
+
+        }catch(error){
+            console.log(error);
+        }
+        const burger= await orderfood("burger",1000);
+        console.log(burger);
+        const pasta= await orderfood("pasta",1000);
+        console.log(pasta);
+     
+    
+    }
+    restraunt();
+    
