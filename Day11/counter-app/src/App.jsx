f355ab8724a28a715074367a20693c422047ccb7
007divyanshu <pandeydivyanshu1509@gmail.com>
2024-12-02 10:26:11 +0530

    Day11

---
+++ Day11/counter-app/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import CounterApp from './Components/CounterApp';
+import ImageApp from './Components/ImageApp';
+
+const App = () => {
+  return (
+    <div>
+      <ImageApp />
+    </div>
+  )
+}
+
+export default App
