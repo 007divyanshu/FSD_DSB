58d52a1f6861b9549b7acba1a1852c6e7c2ac8c7
007divyanshu <pandeydivyanshu1509@gmail.com>
2024-12-04 15:28:23 +0530

    Day11 2.0

--- Day11/counter-app/src/App.jsx
+++ Day11/counter-app/src/App.jsx
@@ -1,13 +1,21 @@
-// eslint-disable-next-line no-unused-vars
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CounterApp from './Components/CounterApp';
 import ImageApp from './Components/ImageApp';
+import MainLayout from './Components/MainLayout'; 
 
 const App = () => {
   return (
-    <div>
-      <ImageApp />
-    </div>
-  )
-}
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path="/counterapp" element={<CounterApp />} />
+          <Route path="/imageapp" element={<ImageApp />} />
+        </Routes>
+      </MainLayout>
+    </Router>
+  );
+};
 
-export default App
+export default App;
