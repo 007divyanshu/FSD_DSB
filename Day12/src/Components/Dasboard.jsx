5c0ec2bfad66426194756d7ccedf260db2f2b43d
007divyanshu <pandeydivyanshu1509@gmail.com>
2024-12-11 15:36:38 +0530

    day12

--- Day12/src/Components/Dasboard.jsx
+++ Day12/src/Components/Dasboard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-
-const Dasboard = () => {
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const DashBoard = ({ regDash }) => {
   return (
     <div>
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12" style={{ backgroundColor: "grey" }}>
+            <h1>Welcome {regDash.name}</h1>
+            <h2>Email ID {regDash.email}</h2>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dasboard
+export default DashBoard;
