b4c6236337efab81436a1e9b0e55eda00d2fe1d0
Divyanshu Pandey <136846389+007divyanshu@users.noreply.github.com>
2024-12-16 10:45:57 +0530

    Update WeatherApp.jsx

--- Day12/src/Components/WeatherApp.jsx
+++ Day12/src/Components/WeatherApp.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const WeatherApp = () => {
     const [city,setcity]=useState();
     const [data,setweather]=useState();
-    const api_key ="72a581a29acbd8f56bd9eed638c30cb0";
+    const api_key ="";
     const fetchdata = () => {
         alert("Weather data");
     };
